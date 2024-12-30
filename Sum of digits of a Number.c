32d2288b11b7863d063ab494632b02ac2aff1031
Dipali Mali <148054174+dipalimali213@users.noreply.github.com>
2024-12-30 10:25:41 +0530

    Add files via upload

---
+++ Sum of digits of a Number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int num,sum = 0;
+	printf("Enter a Number : ");
+	scanf("%d",&num);
+	while(num!=0){
+		sum = sum + (num%10);
+		num = num/10;
+	}
+	printf("The Sum : %d",sum);
+}
